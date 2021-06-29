678159185d130b00a97d41597876f7d8facf5af0
Joel Flores <j_flores88@outlook.com>
2021-06-28 20:03:33 -0700

    Add info function to book constructor prototype

--- index.js
+++ index.js
@@ -6,3 +6,9 @@ function Book(title, author, pages, read){
     this.pages = pages,
     this.read = read
 };
+
+Book.prototype.info = function() {
+    let read;
+    (!read) ? read = 'have not read' : read = 'have read';
+    return `${this.title}, by ${this.author}, ${this.pages} long, ${read}.`
+};
