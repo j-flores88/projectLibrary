92f55935d974a46353bbfbf10ba70706d5d08d05
Joel Flores <j_flores88@outlook.com>
2021-06-28 20:52:56 -0700

    Add forEach loop that displays book on page

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const libDis = document.getElementById('libraryDisplay')
+
 let myLibrary = [];
 
 function Book(title, author, pages, read){
@@ -11,3 +13,14 @@ const thinkAndGrow = new Book('Think And Grow Rich', 'Napolean Hill', 700, true)
 myLibrary.push(thinkAndGrow);
 const sevenHabits = new Book('The 7 Habits of Highly Effective People', 'Stephen R. Covey', 340, false);
 myLibrary.push(sevenHabits);
+
+myLibrary.forEach(book => {
+    let bookCard = document.createElement("div");
+    bookCard.innerHTML = `
+        <div>${book.title}</div>
+        <div>${book.author}</div>
+        <div>${book.pages}</div>
+        <div>${book.read}</div>
+    `
+    libDis.appendChild(bookCard);
+})
