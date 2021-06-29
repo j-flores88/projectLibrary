e6bc01ad34304249782d829d24c2757de922d4af
Joel Flores <j_flores88@outlook.com>
2021-06-28 21:07:19 -0700

    Add readMessage function to Book prototype

--- index.js
+++ index.js
@@ -9,18 +9,25 @@ function Book(title, author, pages, read){
     this.read = read
 };
 
-const thinkAndGrow = new Book('Think And Grow Rich', 'Napolean Hill', 700, true);
+Book.prototype.readMessage = function() {
+    let message;
+    (!this.read) ? message = 'Have Not Read' : message = "Have Read";
+    return message;
+}
+
+const thinkAndGrow = new Book('Think And Grow Rich', 'Napolean Hill', 290, true);
 myLibrary.push(thinkAndGrow);
 const sevenHabits = new Book('The 7 Habits of Highly Effective People', 'Stephen R. Covey', 340, false);
 myLibrary.push(sevenHabits);
 
 myLibrary.forEach(book => {
+    //on submit, clear all old data? or check if it alread exist
     let bookCard = document.createElement("div");
     bookCard.innerHTML = `
         <div>${book.title}</div>
         <div>${book.author}</div>
         <div>${book.pages}</div>
-        <div>${book.read}</div>
+        <div>${book.readMessage()}</div>
     `
     libDis.appendChild(bookCard);
 })
